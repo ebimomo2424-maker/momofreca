783a1ea2fd912fc20d1c61064635522d8ee76b8c
ebimomo2424-maker <ebimomo.2424@gmail.com>
2026-07-20 21:40:14 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,3 +1,24 @@
 const cards = [
 
+{
+character:"ももか",
+title:"いちご",
+subtitle:"かわいいピンク",
+image:"https://placehold.co/300x420/f8c8dc/ffffff?text=Kitty"
+},
+
+{
+character:"だいふく",
+title:"ドーナツ",
+subtitle:"ふわふわ",
+image:"https://placehold.co/300x420/cceeff/ffffff?text=Cinnamoroll"
+},
+
+{
+character:"フォルル",
+title:"ハロウィン",
+subtitle:"いたずら",
+image:"https://placehold.co/300x420/d7c3ff/ffffff?text=Kuromi"
+}
+
 ];

d5f4ad63154fccc60e4b72d217de7be71fd00d76
ebimomo2424-maker <ebimomo.2424@gmail.com>
2026-07-20 21:49:07 +0900

    Add dynamic card rendering to gallery

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const gallery = document.getElementById("gallery");
 
+cards.forEach(card => {
+
+gallery.innerHTML += `
+
+<div class="card">
+
+<img src="${card.image}">
+
+<h3>${card.title}</h3>
+
+<p>${card.subtitle}</p>
+
+</div>
+
+`;
+
+});
